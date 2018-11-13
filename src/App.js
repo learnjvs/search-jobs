6fac489a03da570144523e3ac8ce5366431a8f4c
Mystique <helenk530@yahoo.com>
2018-11-12 21:45:46 -0800

    create inputs, submit button, and set the initial state

--- src/App.js
+++ src/App.js
@@ -3,13 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state= {
+      language: "",
+      city: "",
+      searchResult: []
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  };
+
+  searchJob = () => {
+
+    console.log(this.state);
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edityfrhtyfdhtydfhgtd <code>src/App.js</code> and save to reload.
           </p>
           <a
             className="App-link"
@@ -19,6 +39,21 @@ class App extends Component {
           >
             Learn React
           </a>
+        <input 
+          type="text" 
+          placeholder="language" 
+          name="language"
+          value={this.state.language}
+          onChange={this.handleChange}
+          />
+        <input 
+          type="text" 
+          placeholder="city" 
+          name="city"
+          value={this.state.city}
+          onChange={this.handleChange}
+        />
+        <button onClick={this.searchJob}>submit</button>
         </header>
       </div>
     );

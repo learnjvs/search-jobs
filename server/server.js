ae5df3efc5c1d63dfe767a7ee5ebfea20d94399c
Mystique <helenk530@yahoo.com>
2018-11-20 22:34:08 -0800

    connect to monboDB in server.js

--- server/server.js
+++ server/server.js
@@ -2,8 +2,18 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const jobController = require('./jobController');
+const mongoose = require('mongoose');
 
 const PORT = 3300;
+const url = 'mongodb://searchjob:practice123@ds052408.mlab.com:52408/search-job'
+const db = mongoose.connection;
+
+//connecting to db
+mongoose.connect(url);
+db.once('open', () => {
+console.log('connected to MongoDB...')
+});
+
 
 //app.use(bodyParser.json());  body-parser middleware is bundled in express 4.16.0
 app.use(express.json());

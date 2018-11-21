eb9c96b8a500b6683233f431ebfaa1c866285320
Mystique <helenk530@yahoo.com>
2018-11-21 15:40:48 -0800

    install react-html-parser and parse string description to html react

--- src/Job.js
+++ src/Job.js
@@ -1,19 +1,23 @@
-import React from 'react'
+import React from 'react';
+import ReactHtmlParser from 'react-html-parser';
+import JobDescription from './JobDescription';
 
-const Job = ({ date, title, location, type, company, company_logo }) => {
+const Job = ({ date, title, location, type, company, company_logo, description }) => {
   // createdAt, title, location, type, company, company_logo
  
   let logo = company_logo ? <img src={company_logo} alt={`${company} logo`} /> : null;
 
   return (
     <div>
-      <ul>
+      <ul id="shortDescription">
         {logo}
         <li>{title}</li>
         <li>{location}</li>
         <li>{type}</li>
         <li>{company}</li>
         <li>{date}</li>
+        <li>{ReactHtmlParser(description)}</li>
+        {/* <JobDescription /> */}
       </ul>
     </div>
   )

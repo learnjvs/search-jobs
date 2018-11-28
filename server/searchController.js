dfa98852d73f3e389509e769e9751d61067caa32
Mystique <helenk530@yahoo.com>
2018-11-28 00:00:19 -0800

    add List.create

--- server/searchController.js
+++ server/searchController.js
@@ -7,10 +7,15 @@ searchController.fetchJobsList = async (req, res, next) => {
   const url = `https://jobs.github.com/positions.json?description=${language}&location=${city}`;
 
   try {
+    // fetch data from github
     const response = await fetch(url);
     const searchResult = await response.json();
     console.log('FETCH JOB LIST RUNNING');
+    // store returned data in the database and res.locals
     res.locals.searchResult = searchResult;
+    List.create({language, city}, (err) => {
+      if (err) return res.status(500).send(err);
+    })
     next();
   } catch (e) {
     return res.status(500).send(e)
@@ -21,11 +26,13 @@ searchController.jobsList= (req, res, next) => {
   const { language, city } = req.body;
   List.find({ language, city }, (err, jobsList) => {
     if (jobsList.length === 0) {
-      console.log('INSIDE IF,', jobsList)
+      console.log('INSIDE JOBSLIST IF,', jobsList)
       next();
     }
-    else  console.log('FIND FROM DB'); res.locals.jobsList = jobsList;
+    else  console.log('FIND FROM DB', jobsList); res.locals.jobsList = jobsList;
   })
 }
+
+
 
 module.exports = searchController;
